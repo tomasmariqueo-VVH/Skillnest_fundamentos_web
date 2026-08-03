64781f27b65da8f0d290a3e5f5232410f94e1fa6
tomasmariqueo-VVH <tomasmariqueo@liceovvh.cl>
2026-08-03 15:39:57 -0400

    cajas

--- STACK_JS/12_eventos_onclick_JS/static/js/script.js
+++ STACK_JS/12_eventos_onclick_JS/static/js/script.js
@@ -7,7 +7,8 @@ document.getElementById("colorButton1").addEventListener("click", function () {
 function cambiarColor1() {
     let colores = ["#1613d4", "#359eff", "#41347c", "#5463f0", "#008cff", "#037fb1"];
     let colorAleatorio = colores[Math.floor(Math.random() * colores.length)];
-    document.body.style.backgroundColor = colorAleatorio;
+    const elemento = document.querySelector("body")
+    elemento.style.backgroundColor = colorAleatorio;
 }
 
 //BOTÓN 2
@@ -17,9 +18,10 @@ document.getElementById("colorButton2").addEventListener("click", function () {
 
 // Función que cambia el color de fondo al hacer clic
 function cambiarColor2() {
-    let colores = ["#ff4747", "#960505", "#ffa65d", "#5c0000", "#b63939", "#ca2626"];
+    let colores = ["#ff4747", "#960505", "#e00000", "#5c0000", "#b63939", "#ca2626"];
     let colorAleatorio = colores[Math.floor(Math.random() * colores.length)];
-    document.body.style.backgroundColor = colorAleatorio;
+    const elemento = document.querySelector("body")
+    elemento.style.backgroundColor = colorAleatorio;
 }
 
 //BOTÓN 3
@@ -31,5 +33,12 @@ document.getElementById("colorButton3").addEventListener("click", function () {
 function cambiarColor3() {
     let colores = ["#b3ff00", "#fffb25", "#b8bb00", "#ffd900", "#aabd42", "#ffef0f"];
     let colorAleatorio = colores[Math.floor(Math.random() * colores.length)];
-    document.body.style.backgroundColor = colorAleatorio;
+    const elemento = document.querySelector("#caja1")
+    elemento.style.backgroundColor = colorAleatorio;
 }
+
+//SECCIONES
+
+
+
+//un ID se selecciona con un #
